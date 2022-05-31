62c14b88c195003701990a4911f757de233cf27d
Max Zhao <mdcw1103@gmail.com>
2022-06-01 00:06:43 +0800

    feat: add run mode select

--- packages/renderer/src/views/Home/OverView.vue
+++ packages/renderer/src/views/Home/OverView.vue
@@ -9,7 +9,10 @@
         embedded
         title="运行"
       >
-        <n-space vertical>
+        <n-space
+          vertical
+          :size="12"
+        >
           <div>
             <label class="w-24 inline-block">运行状态：</label>
             <n-switch size="medium">
@@ -20,6 +23,22 @@
                 已停止
               </template>
             </n-switch>
+          </div>
+          <div>
+            <label class="w-24 inline-block">运行模式：</label>
+            <n-radio-group
+              default-value="direct"
+              :value="activatedRunMode"
+              @update:value="handleRunModeChange"
+              size="small"
+            >
+              <n-radio-button
+                v-for="(model, index) in runModels"
+                :key="index"
+                :label="model.label"
+                :value="model.value"
+              />
+            </n-radio-group>
           </div>
           <div class="flex items-center">
             <label class="w-24 inline-block">本地IP：</label>
@@ -95,19 +114,41 @@
 
 <script setup lang="ts">
 import {
-  NIcon, NLayout, NLayoutSider, NMenu, NCard, NSwitch, NText, NSpace, NButton, NGrid, NGridItem,
-  useMessage, NEmpty,
+  NIcon, NCard, NSwitch, NText, NSpace, NButton, NGrid, NGridItem,
+  useMessage, NEmpty, NRadioGroup, NRadioButton,
 } from 'naive-ui'
-
-import {
-  Dashboard, ServerProxy, Document, Settings, Copy, Rule,
-} from '@vicons/carbon'
+import { Copy } from '@vicons/carbon'
+import { ref } from 'vue'
 
 const message = useMessage()
 function handleClipboardCopy(text: string) {
   window.copyTextToClipboard(text)
   message.success('Copy Success')
 }
+
+type RunMode = {
+  label: string,
+  value: 'direct' | 'rule' | 'global'
+}
+
+const runModels: RunMode[] = [{
+  label: '直连',
+  value: 'direct',
+}, {
+  label: '规则',
+  value: 'rule',
+}, {
+  label: '全局',
+  value: 'global',
+}]
+
+const activatedRunMode = ref<RunMode['value']>('direct')
+
+function handleRunModeChange(mode: RunMode['value']) {
+  activatedRunMode.value = mode
+  console.log(mode)
+}
+
 </script>
 
 <style scoped>
