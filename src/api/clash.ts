fdc416cf2fd1c63d93b31516eee6044ccfe3ece7
Max Zhao <mdcw1103@gmail.com>
2022-07-01 01:32:25 +0800

    feat: add more net api of clash

--- src/api/clash.ts
+++ src/api/clash.ts
@@ -13,6 +13,71 @@ export async function patchBaseConfig(config: Partial<BaseClashConfig>) {
   return await axiosInstance.patch('/configs', config)
 }
 
+export async function proxies() {
+  return await axiosInstance.get('/proxies')
+}
+
+export async function proxy(name: string) {
+  return await axiosInstance.get(`/proxies/${name}`)
+}
+
+export async function selectProxy(name: string) {
+  return await axiosInstance.put(`/proxies/${name}`)
+}
+
+export async function proxyDelay(name: string) {
+  return await axiosInstance.get(`/proxies/${name}/delay`)
+}
+
+export async function rules() {
+  return await axiosInstance.get('/rules')
+}
+
+export async function connections() {
+  return await axiosInstance.get('/connections')
+}
+
+export async function closeAllConnections() {
+  return await axiosInstance.delete('/connections')
+}
+
+export async function closeConnection(connectionId: string) {
+  return await axiosInstance.delete(`/connections/${connectionId}`)
+}
+
+export async function version() {
+  return await axiosInstance.get('/version')
+}
+
+export async function proxiesOfProviders() {
+  return await axiosInstance.get('/providers/proxies')
+}
+
+export async function proxiesOfProvider(providerName: string) {
+  return await axiosInstance.get(`/providers/proxies/${providerName}`)
+}
+
+export async function selectProxyProvider(providerName: string) {
+  return await axiosInstance.put(`/providers/proxies/${providerName}`)
+}
+
+export async function healthcheckProxyProiver(providerName: string) {
+  return await axiosInstance.get(`/providers/proxies/${providerName}/healthcheck`)
+}
+
 export default {
   baseConfig,
+  putBaseConfig,
+  patchBaseConfig,
+  proxies,
+  selectProxy,
+  proxyDelay,
+  connections,
+  closeAllConnections,
+  closeConnection,
+  version,
+  proxiesOfProviders,
+  proxiesOfProvider,
+  selectProxyProvider,
+  healthcheckProxyProiver,
 }
