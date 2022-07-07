4b4c49dca197f59ffc6f06e9353d8efc3e5613cd
Max Zhao <mdcw1103@gmail.com>
2022-07-07 14:07:33 +0800

    chore: config resolve.alias for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,15 @@
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     exclude: ['node_modules', 'dist', '.idea', '.git', '.cache', 'test'],
+  },
+  resolve: {
+    alias: {
+      '@/render': fileURLToPath(new URL('./src', import.meta.url)),
+      '@/share': fileURLToPath(new URL('./packages/share', import.meta.url)),
+      '@/main': fileURLToPath(new URL('./electron/main', import.meta.url)),
+      '@/preload': fileURLToPath(new URL('./electron/preload', import.meta.url)),
+    },
   },
 })
